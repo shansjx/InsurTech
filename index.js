5d099284cf9a99502fe3f71fee267abce07b49a5
shansjx <sohshana0456@gmail.com>
2022-11-14 18:16:47 +0800

    adding handlebars, favicon not loading

--- index.js
+++ index.js
@@ -1,15 +1,30 @@
 
 const express = require("express");
+const exphbs = require("express-handlebars");
 const bodyParser = require("body-parser");
 
 const app = express();
+const port = 3000;
+
+//bodyparser
 app.use(bodyParser.urlencoded({extended:true}));
 
+//sets hbs config
+app.engine('handlebars', exphbs.engine({
+    layoutsDir: __dirname + '/views/layouts',
+    partialsDir: __dirname + '/views/partials/'
+}));
+//set app to use the hbs engine
+app.set('view engine','handlebars');
+
+//when user access the root url
 app.get("/",function(req,res)
 {
-    res.send("<h1>Hello world!</h1>");
+    res.render('main', {layout : 'index'});
 });
 
-app.listen(3000, function(){
-    console.log("App listening to port 3000")
+
+//app listening to port
+app.listen(port, function(){
+    console.log(`App listening to port ${port}`)
 });
